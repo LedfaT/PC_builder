e0e9e3dd5ef322eff20a8f954987074c76d980c3
David Zaitsev <davidgalanix@gmail.com>
2025-06-19 14:00:04 +0000

    add indexes for all dependencies

--- backend/src/types/dependencyTypes.ts
+++ backend/src/types/dependencyTypes.ts
@@ -19,3 +19,48 @@ export const ssdDependencyTypes = {
   SsdService: Symbol.for("SsdService"),
   SsdController: Symbol.for("SsdController"),
 };
+
+export const ramDependencyTypes = {
+  RamService: Symbol.for("RamService"),
+  RamController: Symbol.for("RamController"),
+};
+
+export const hddDependencyTypes = {
+  HddService: Symbol.for("HddService"),
+  HddController: Symbol.for("HddController"),
+};
+
+export const wifiDependencyTypes = {
+  WifiService: Symbol.for("WifiService"),
+  WifiController: Symbol.for("WifiController"),
+};
+
+export const bluetoothDependencyTypes = {
+  BluetoothService: Symbol.for("BluetoothService"),
+  BluetoothController: Symbol.for("BluetoothController"),
+};
+
+export const powerSupplyDependencyTypes = {
+  PowerSupplyService: Symbol.for("PowerSupplyService"),
+  PowerSupplyController: Symbol.for("PowerSupplyController"),
+};
+
+export const motherboardDependencyTypes = {
+  MotherboardService: Symbol.for("MotherboardService"),
+  MotherboardController: Symbol.for("MotherboardController"),
+};
+
+export const coolingDependencyTypes = {
+  CoolingService: Symbol.for("CoolingService"),
+  CoolingController: Symbol.for("CoolingController"),
+};
+
+export const towerDependencyTypes = {
+  TowerService: Symbol.for("TowerService"),
+  TowerController: Symbol.for("TowerController"),
+};
+
+export const waterCoolingDependencyTypes = {
+  WaterCoolingService: Symbol.for("WaterCoolingService"),
+  WaterCoolingController: Symbol.for("WaterCoolingController"),
+};
