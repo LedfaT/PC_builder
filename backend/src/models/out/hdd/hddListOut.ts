a7f5a9910bd6e06745798d8a4dad0d3caadfa820
David Zaitsev <davidgalanix@gmail.com>
2025-06-20 08:06:22 +0000

    add hdd models

--- backend/src/models/out/hdd/hddListOut.ts
+++ backend/src/models/out/hdd/hddListOut.ts
@@ -1,15 +1,17 @@
-module.exports = class HddListOut {
-  id;
-  title;
-  memory_quantity;
-  cost;
-  reading_speed;
-  write_speed;
-  createdAt;
-  updatedAt;
-  description;
-  image;
-  constructor(hdd) {
+import { HDD } from "@prisma/client";
+
+export default class HddListOut {
+  id: number;
+  title: string;
+  memory_quantity: string;
+  cost: number | null;
+  reading_speed: string;
+  write_speed: string;
+  createdAt: Date | null;
+  updatedAt: Date | null;
+  description: string | null;
+  image: string | null;
+  constructor(hdd: HDD) {
     this.id = hdd.id;
     this.cost = hdd.cost;
     this.memory_quantity = hdd.memory_quantity;
@@ -21,4 +23,4 @@ module.exports = class HddListOut {
     this.createdAt = hdd.createdAt;
     this.updatedAt = hdd.updatedAt;
   }
-};
+}
---
+++ backend/src/models/out/hdd/hddOut.ts
@@ -0,0 +1,26 @@
+import { HDD } from "@prisma/client";
+
+export default class HddOut {
+  id: number;
+  title: string;
+  memory_quantity: string;
+  cost: number | null;
+  reading_speed: string;
+  write_speed: string;
+  createdAt: Date | null;
+  updatedAt: Date | null;
+  description: string | null;
+  image: string | null;
+  constructor(hdd: HDD) {
+    this.id = hdd.id;
+    this.cost = hdd.cost;
+    this.memory_quantity = hdd.memory_quantity;
+    this.title = hdd.title;
+    this.image = hdd.image;
+    this.description = hdd.description;
+    this.reading_speed = hdd.reading_speed;
+    this.write_speed = hdd.write_speed;
+    this.createdAt = hdd.createdAt;
+    this.updatedAt = hdd.updatedAt;
+  }
+}
