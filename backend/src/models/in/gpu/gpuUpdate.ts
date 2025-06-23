8f32b6dd39c1cb1f439c40937a6024a9e2449b65
David Zaitsev <davidgalanix@gmail.com>
2025-06-23 08:06:59 +0000

    add in gpu in models

---
+++ backend/src/models/in/gpu/gpuUpdate.ts
@@ -0,0 +1,27 @@
+import { GPU } from "@prisma/client";
+
+export default class GpuUpdate {
+  title: string;
+  cores: string;
+  cost: number | null;
+  threads: string;
+  vram_quantity: string;
+  cache: string;
+  clock: string;
+  vram_type: string;
+  description: string | null;
+  image: string | null;
+
+  constructor(gpu: GPU) {
+    this.cost = gpu.cost;
+    this.cores = gpu.cores;
+    this.title = gpu.title;
+    this.description = gpu.description;
+    this.image = gpu.image;
+    this.threads = gpu.threads;
+    this.vram_quantity = gpu.vram_quantity;
+    this.cache = gpu.cache;
+    this.clock = gpu.clock;
+    this.vram_type = gpu.vram_type;
+  }
+}
