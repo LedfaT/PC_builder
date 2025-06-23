adbff3c97c46581649424cab7c4bfcfe4b890e30
David Zaitsev <davidgalanix@gmail.com>
2025-06-23 08:31:24 +0000

    add in tower in models

---
+++ backend/src/models/in/tower/towerCreate.ts
@@ -0,0 +1,21 @@
+import { FanType, Tower, TypeSizeTower } from "@prisma/client";
+
+export default class TowerCreate {
+  title: string;
+  fan_included: boolean;
+  cost: number | null;
+  type_size: TypeSizeTower;
+  fan_type: FanType;
+  description: string | null;
+  image: string | null;
+
+  constructor(tower: Tower) {
+    this.cost = tower.cost;
+    this.fan_included = tower.fan_included;
+    this.title = tower.title;
+    this.image = tower.image;
+    this.description = tower.description;
+    this.type_size = tower.type_size;
+    this.fan_type = tower.fan_type;
+  }
+}
