aecfaeeff98ead71ec94e6cc731804fb78e0521e
David Zaitsev <davidgalanix@gmail.com>
2025-06-23 08:34:54 +0000

    add in wifi module in models

---
+++ backend/src/models/in/wifiModule/wifiModuleCreate.ts
@@ -0,0 +1,16 @@
+import { WifiModule } from "@prisma/client";
+export default class WifiModuleCreate {
+  title: string;
+  generation: string;
+  image: string | null;
+  description: string | null;
+  cost: number | null;
+
+  constructor(wifi: WifiModule) {
+    this.cost = wifi.cost;
+    this.image = wifi.image;
+    this.description = wifi.description;
+    this.generation = wifi.generation;
+    this.title = wifi.title;
+  }
+}
---
+++ backend/src/models/in/wifiModule/wifiModuleUpdate.ts
@@ -0,0 +1,17 @@
+import { WifiModule } from "@prisma/client";
+
+export default class WifiModuleUpdate {
+  title: string;
+  generation: string;
+  image: string | null;
+  description: string | null;
+  cost: number | null;
+
+  constructor(wifi: WifiModule) {
+    this.cost = wifi.cost;
+    this.image = wifi.image;
+    this.description = wifi.description;
+    this.generation = wifi.generation;
+    this.title = wifi.title;
+  }
+}
