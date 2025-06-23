e1f18e8d51ea8dc86af1e9d033a771e47817d81a
David Zaitsev <davidgalanix@gmail.com>
2025-06-23 08:33:25 +0000

    add in water coooling system in models

---
+++ backend/src/models/in/waterCoolingSystem/waterCoolingSystemCreate.ts
@@ -0,0 +1,19 @@
+import { TypeSizeWaterCoolingSystem, WaterCoolingSystem } from "@prisma/client";
+
+export default class WaterCoolingSystemCreate {
+  title: string;
+  cost: number | null;
+  type_size: TypeSizeWaterCoolingSystem;
+  heat_removal: string | null;
+  description: string | null;
+  image: string | null;
+
+  constructor(waterCoolingSystem: WaterCoolingSystem) {
+    this.cost = waterCoolingSystem.cost;
+    this.title = waterCoolingSystem.title;
+    this.description = waterCoolingSystem.description;
+    this.image = waterCoolingSystem.image;
+    this.type_size = waterCoolingSystem.type_size;
+    this.heat_removal = waterCoolingSystem.heat_removal;
+  }
+}
