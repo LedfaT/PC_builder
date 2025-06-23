b4db6501c396a46ef33505be618fa2079478c493
David Zaitsev <davidgalanix@gmail.com>
2025-06-23 08:07:57 +0000

    add in hdd in models

---
+++ backend/src/models/in/hdd/hddCreate.ts
@@ -0,0 +1,21 @@
+import { HDD } from "@prisma/client";
+
+export default class HddCreate {
+  title: string;
+  memory_quantity: string;
+  cost: number | null;
+  reading_speed: string;
+  write_speed: string;
+  description: string | null;
+  image: string | null;
+
+  constructor(hdd: HDD) {
+    this.cost = hdd.cost;
+    this.memory_quantity = hdd.memory_quantity;
+    this.title = hdd.title;
+    this.image = hdd.image;
+    this.description = hdd.description;
+    this.reading_speed = hdd.reading_speed;
+    this.write_speed = hdd.write_speed;
+  }
+}
