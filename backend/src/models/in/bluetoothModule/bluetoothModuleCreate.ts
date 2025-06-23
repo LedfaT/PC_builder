45bd4bca9a38c69b4a07f81afbd7c4ac51fc459e
David Zaitsev <davidgalanix@gmail.com>
2025-06-23 08:02:14 +0000

    add in bluetooth in models

---
+++ backend/src/models/in/bluetoothModule/bluetoothModuleCreate.ts
@@ -0,0 +1,16 @@
+import { BluetoothModule } from "@prisma/client";
+
+export default class BluetoothModuleCreate {
+  title: string;
+  generation: string;
+  cost: number | null;
+  description: string | null;
+  image: string | null;
+  constructor(bluetooth: BluetoothModule) {
+    this.cost = bluetooth.cost;
+    this.generation = bluetooth.generation;
+    this.image = bluetooth.image;
+    this.description = bluetooth.description;
+    this.title = bluetooth.title;
+  }
+}
