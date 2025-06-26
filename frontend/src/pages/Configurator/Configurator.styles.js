5a7ed20ca5ca4609544502e63332934e87a0d6f3
Valeriia Martynenko <valerriamartynenko@gmail.com>
2025-06-26 19:15:52 +0300

    feat: додано сторінку Components

---
+++ frontend/src/pages/Configurator/Configurator.styles.js
@@ -0,0 +1,7 @@
+export const configuratorPageStyles = {
+    configuratorPage: "bg-gray-50 min-h-screen flex flex-col",
+    mainGrid: "ml-[5%] mr-[5%] flex gap-[16px]",
+    leftColumn: "flex flex-col gap-4 flex-grow",
+    rightColumn: "flex-shrink-0", 
+  };
+  
