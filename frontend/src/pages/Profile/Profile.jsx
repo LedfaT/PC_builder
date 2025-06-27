5cb889f60961f7151b7182b4277eb0382ea3b4d4
GetApple <test@MacBook-2.local>
2025-06-27 10:07:54 +0300

    Додано сторінку профілю з перевіркою авторизації та двома секціями

---
+++ frontend/src/pages/Profile/Profile.jsx
@@ -0,0 +1,26 @@
+import { profilePageStyles } from "./Profile.styles";
+import { useNavigate } from "react-router-dom";
+
+import ProfileFirstSection from "@/components/ui/ProfileElements/ProfileFirstSection/FirstSection";
+import ProfileSecondSection from "@/components/ui/ProfileElements/ProfileSecondSection/SecondSection";
+import { useContext, useEffect } from "react";
+import { Context } from "@/contextProvider";
+const Profile = () => {
+  const { store } = useContext(Context);
+  const auth = store.getData().isAuth;
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!auth) {
+      navigate("/signin");
+      return;
+    }
+  }, []);
+  return (
+    <div className={profilePageStyles.profilePage}>
+      <ProfileFirstSection />
+      <ProfileSecondSection />
+    </div>
+  );
+};
+
+export default Profile;
