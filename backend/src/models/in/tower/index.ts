d319ae68fc7da5627d16e4b3f9bc9f21a33a6dc0
Danil Drygalka <h3hh33hhh333hhhh3333@gmail.com>
2025-06-29 14:36:49 +0000

    add tower service

--- backend/src/models/in/tower/index.ts
+++ backend/src/models/in/tower/index.ts
@@ -1,19 +1,15 @@
 import { FanType, Tower, TypeSizeTower } from "@prisma/client";
 
-export default class TowerListOut {
-  id: number;
+export default class TowerCreate {
   title: string;
   fan_included: boolean;
   cost: number | null;
   type_size: TypeSizeTower;
   fan_type: FanType;
-  createdAt: Date | null;
-  updatedAt: Date | null;
   description: string | null;
   image: string | null;
 
   constructor(tower: Tower) {
-    this.id = tower.id;
     this.cost = tower.cost;
     this.fan_included = tower.fan_included;
     this.title = tower.title;
@@ -21,7 +17,7 @@ export default class TowerListOut {
     this.description = tower.description;
     this.type_size = tower.type_size;
     this.fan_type = tower.fan_type;
-    this.createdAt = tower.createdAt;
-    this.updatedAt = tower.updatedAt;
   }
 }
+
+export type TTowerUpdate = Partial<TowerCreate>;
