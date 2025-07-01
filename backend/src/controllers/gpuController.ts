70bcc0721fd770315eb4d68977ddefb41c9ddb83
Danil Drygalka <h3hh33hhh333hhhh3333@gmail.com>
2025-07-01 19:25:19 +0000

    add gpu controller

--- backend/src/controllers/gpuController.ts
+++ backend/src/controllers/gpuController.ts
@@ -1,12 +1,80 @@
-import { gpuDependencyTypes } from "@ownTypes/dependencyTypes";
-import { GpuService } from "@services/gpuService";
+import { Request, Response, NextFunction } from "express";
 import { inject, injectable } from "inversify";
+
+import { gpuDependencyTypes } from "@ownTypes/dependencyTypes";
+import GpuCreate, { TGpuUpdate } from "@models/in/gpu";
+import GpuService from "@services/gpuService";
 
 @injectable()
 export class GpuController {
-  gpuService: GpuService;
+  constructor(
+    @inject(gpuDependencyTypes.GpuService)
+    private readonly gpuService: GpuService
+  ) {}
 
-  constructor(@inject(gpuDependencyTypes.GpuService) gpuService: GpuService) {
-    this.gpuService = gpuService;
+  async createGpu(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const newGpu = new GpuCreate(req.body);
+      await this.gpuService.create(newGpu);
+      res.json({ message: "GPU created" });
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async getGpu(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+      const { id } = req.params;
+      const gpu = await this.gpuService.getGpuById(+id);
+      res.json(gpu);
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async updateGpu(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const { id } = req.params;
+      const updateData: TGpuUpdate = req.body;
+      await this.gpuService.update(+id, updateData);
+      res.json({ message: "GPU updated" });
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async deleteGpu(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const { id } = req.params;
+      await this.gpuService.delete(+id);
+      res.json({ message: "GPU deleted successfully" });
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async getAllGpus(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const gpus = await this.gpuService.getAllGpus(req.query);
+      res.json(gpus);
+    } catch (e) {
+      next(e);
+    }
   }
 }
