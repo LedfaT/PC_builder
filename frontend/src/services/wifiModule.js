feaa69398443d25c24a30f69c7844cef88d21f5e
GetApple <test@MacBook-2.local>
2025-07-01 09:06:24 +0300

    Додано сервіс WifiModuleService для CRUD-операцій з WiFi модулями

---
+++ frontend/src/services/wifiModule.js
@@ -0,0 +1,25 @@
+import $api from "../http";
+
+export default class WifiModuleService {
+  static async createWifiModule(payload) {
+    return $api.post("/api/wifi-module", payload);
+  }
+
+  static async getAllWifiModules(page, limit, search = "") {
+    return $api.get(
+      /api/wifi-module/list?page=${page}&limit=${limit}&search=${search}
+    );
+  }
+
+  static async getWifiModuleById(id) {
+    return $api.get(`/api/wifi-module/${id}`);
+  }
+
+  static async updateWifiModule(id, payload) {
+    return $api.patch(`/api/wifi-module/${id}`, payload);
+  }
+
+  static async deleteWifiModule(id) {
+    return $api.delete(`/api/wifi-module/${id}`);
+  }
+}
