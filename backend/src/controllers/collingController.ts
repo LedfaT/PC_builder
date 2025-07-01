f1e7c7fed76f6839aea9a3ca0bda02cdd30f47df
Danil Drygalka <h3hh33hhh333hhhh3333@gmail.com>
2025-07-01 19:15:54 +0000

    add cooling system controller

--- backend/src/controllers/collingController.ts
+++ backend/src/controllers/collingController.ts
@@ -1,14 +1,88 @@
-import { coolingDependencyTypes } from "@ownTypes/dependencyTypes";
-import { CoolingService } from "@services/coolingService";
+import { Request, Response, NextFunction } from "express";
 import { inject, injectable } from "inversify";
 
+import { coolingDependencyTypes } from "@ownTypes/dependencyTypes";
+import CoolingSystemCreate, {
+  TCoolingSystemUpdate,
+} from "@models/in/coolingSystem";
+import CoolingSystemService from "@services/coolingService";
+
 @injectable()
-export class Coolingcontroller {
-  coolingService: CoolingService;
+export class CoolingSystemController {
   constructor(
     @inject(coolingDependencyTypes.CoolingService)
-    coolingService: CoolingService
-  ) {
-    this.coolingService = coolingService;
+    private readonly coolingSystemService: CoolingSystemService
+  ) {}
+
+  async createCoolingSystem(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const newCoolingSystem = new CoolingSystemCreate(req.body);
+      await this.coolingSystemService.create(newCoolingSystem);
+      res.json({ message: "Cooling system created" });
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async getCoolingSystem(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const { id } = req.params;
+      const coolingSystem =
+        await this.coolingSystemService.getCoolingSystemById(+id);
+      res.json(coolingSystem);
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async updateCoolingSystem(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const { id } = req.params;
+      const updateData: TCoolingSystemUpdate = req.body;
+      await this.coolingSystemService.update(+id, updateData);
+      res.json({ message: "Cooling system updated" });
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async deleteCoolingSystem(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const { id } = req.params;
+      await this.coolingSystemService.delete(+id);
+      res.json({ message: "Cooling system deleted successfully" });
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async getAllCoolingSystems(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const coolingSystems =
+        await this.coolingSystemService.getAllCoolingSystems(req.query);
+      res.json(coolingSystems);
+    } catch (e) {
+      next(e);
+    }
   }
 }
