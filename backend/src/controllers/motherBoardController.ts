1f4357ec54528bd5b39c2653caf5b7207488a6c9
Danil Drygalka <h3hh33hhh333hhhh3333@gmail.com>
2025-07-01 19:30:50 +0000

    add motherboard controller

--- backend/src/controllers/motherBoardController.ts
+++ backend/src/controllers/motherBoardController.ts
@@ -1,15 +1,86 @@
-import { motherboardDependencyTypes } from "@ownTypes/dependencyTypes";
-import { MotherboardService } from "@services/motherboardService";
+import { Request, Response, NextFunction } from "express";
 import { inject, injectable } from "inversify";
+
+import { motherboardDependencyTypes } from "@ownTypes/dependencyTypes";
+import MotherboardCreate, { TMotherboardUpdate } from "@models/in/motherboard";
+import MotherboardService from "@services/motherboardService";
 
 @injectable()
 export class MotherboardController {
-  motherboardService: MotherboardService;
-
   constructor(
     @inject(motherboardDependencyTypes.MotherboardService)
-    motherboardService: MotherboardService
-  ) {
-    this.motherboardService = motherboardService;
+    private readonly motherboardService: MotherboardService
+  ) {}
+
+  async createMotherboard(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const newMotherboard = new MotherboardCreate(req.body);
+      await this.motherboardService.create(newMotherboard);
+      res.json({ message: "Motherboard created" });
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async getMotherboard(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const { id } = req.params;
+      const motherboard = await this.motherboardService.getMotherboardById(+id);
+      res.json(motherboard);
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async updateMotherboard(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const { id } = req.params;
+      const updateData: TMotherboardUpdate = req.body;
+      await this.motherboardService.update(+id, updateData);
+      res.json({ message: "Motherboard updated" });
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async deleteMotherboard(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const { id } = req.params;
+      await this.motherboardService.delete(+id);
+      res.json({ message: "Motherboard deleted successfully" });
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async getAllMotherboards(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const motherboards = await this.motherboardService.getAllMotherboards(
+        req.query
+      );
+      res.json(motherboards);
+    } catch (e) {
+      next(e);
+    }
   }
 }
