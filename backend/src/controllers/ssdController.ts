10221c70c37080fa34606d7ee0a4ca70cfd1129e
Danil Drygalka <h3hh33hhh333hhhh3333@gmail.com>
2025-07-01 19:43:41 +0000

    add ssd controller

--- backend/src/controllers/ssdController.ts
+++ backend/src/controllers/ssdController.ts
@@ -1,12 +1,80 @@
-import { ssdDependencyTypes } from "@ownTypes/dependencyTypes";
-import { SsdService } from "@services/ssdService";
+import { Request, Response, NextFunction } from "express";
 import { inject, injectable } from "inversify";
+
+import { ssdDependencyTypes } from "@ownTypes/dependencyTypes";
+import SsdCreate, { TSsdUpdate } from "@models/in/ssd";
+import SsdService from "@services/ssdService";
 
 @injectable()
 export class SsdController {
-  ssdService: SsdService;
+  constructor(
+    @inject(ssdDependencyTypes.SsdService)
+    private readonly ssdService: SsdService
+  ) {}
 
-  constructor(@inject(ssdDependencyTypes.SsdService) ssdService: SsdService) {
-    this.ssdService = ssdService;
+  async createSsd(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const newSsd = new SsdCreate(req.body);
+      await this.ssdService.create(newSsd);
+      res.json({ message: "SSD created" });
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async getSsd(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+      const { id } = req.params;
+      const ssd = await this.ssdService.getSsdById(+id);
+      res.json(ssd);
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async updateSsd(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const { id } = req.params;
+      const updateData: TSsdUpdate = req.body;
+      await this.ssdService.update(+id, updateData);
+      res.json({ message: "SSD updated" });
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async deleteSsd(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const { id } = req.params;
+      await this.ssdService.delete(+id);
+      res.json({ message: "SSD deleted successfully" });
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async getAllSsds(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const ssds = await this.ssdService.getAllSsds(req.query);
+      res.json(ssds);
+    } catch (e) {
+      next(e);
+    }
   }
 }
