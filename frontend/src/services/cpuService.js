245b9ac0b16f8198186ec08fb9d0001e155adee3
GetApple <test@MacBook-2.local>
2025-07-01 08:54:59 +0300

    Додано сервіс CpuService для CRUD-операцій з процесорами (CPU)

---
+++ frontend/src/services/cpuService.js
@@ -0,0 +1,25 @@
+import $api from "../http";
+
+export default class CpuService {
+  static async createCpu(payload) {
+    return $api.post("/api/CPU", payload);
+  }
+
+  static async getAllCpus(page, limit, search = "") {
+    return $api.get(
+      /api/CPU/list?page=${page}&limit=${limit}&search=${search}
+    );
+  }
+
+  static async getCpuById(id) {
+    return $api.get(`/api/CPU/${id}`);
+  }
+
+  static async updateCpu(id, payload) {
+    return $api.patch(`/api/CPU/${id}`, payload);
+  }
+
+  static async deleteCpu(id) {
+    return $api.delete(`/api/CPU/${id}`);
+  }
+}
