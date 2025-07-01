a88bed7fc70f6fc9bf1dca19d5127629c1b1f0c6
Danil Drygalka <h3hh33hhh333hhhh3333@gmail.com>
2025-07-01 19:45:14 +0000

    add tower controller

--- backend/src/controllers/towerController.ts
+++ backend/src/controllers/towerController.ts
@@ -1,14 +1,84 @@
-import { towerDependencyTypes } from "@ownTypes/dependencyTypes";
-import { TowerService } from "@services/towerService";
+import { Request, Response, NextFunction } from "express";
 import { inject, injectable } from "inversify";
+
+import { towerDependencyTypes } from "@ownTypes/dependencyTypes";
+import TowerCreate, { TTowerUpdate } from "@models/in/tower";
+import TowerService from "@services/towerService";
 
 @injectable()
 export class TowerController {
-  towerService: TowerService;
-
   constructor(
-    @inject(towerDependencyTypes.TowerService) towerService: TowerService
-  ) {
-    this.towerService = towerService;
+    @inject(towerDependencyTypes.TowerService)
+    private readonly towerService: TowerService
+  ) {}
+
+  async createTower(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const newTower = new TowerCreate(req.body);
+      await this.towerService.create(newTower);
+      res.json({ message: "Tower created" });
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async getTower(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const { id } = req.params;
+      const tower = await this.towerService.getTowerById(+id);
+      res.json(tower);
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async updateTower(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const { id } = req.params;
+      const updateData: TTowerUpdate = req.body;
+      await this.towerService.update(+id, updateData);
+      res.json({ message: "Tower updated" });
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async deleteTower(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const { id } = req.params;
+      await this.towerService.delete(+id);
+      res.json({ message: "Tower deleted successfully" });
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async getAllTowers(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const towers = await this.towerService.getAllTowers(req.query);
+      res.json(towers);
+    } catch (e) {
+      next(e);
+    }
   }
 }
