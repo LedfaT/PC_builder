4138e657bb02a1e3546ba48f100e68a318f5cd65
Danil Drygalka <h3hh33hhh333hhhh3333@gmail.com>
2025-07-16 22:10:08 +0300

    push

--- backend/src/services/mailService.ts
+++ backend/src/services/mailService.ts
@@ -1,4 +1,34 @@
 import { injectable } from "inversify";
+import nodemailer, { Transporter } from "nodemailer";
 
 @injectable()
-export class MailService {}
+export class MailService {
+  private transporter: Transporter;
+
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: parseInt(process.env.SMTP_PORT || "587", 10),
+      secure: false,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASSWORD,
+      },
+    });
+  }
+
+  async sendActivationMail(to: string, link: string) {
+    await this.transporter.sendMail({
+      from: process.env.SMTP_USER,
+      to,
+      subject: "Account activation on " + process.env.API_URL,
+      text: "",
+      html: `
+          <div>
+              <h1>To activate your account go to link</h1>
+              <a href="${link}">${link}</a>
+          </div>
+          `,
+    });
+  }
+}
